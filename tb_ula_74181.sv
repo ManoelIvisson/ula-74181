64720710996cae92f57d24000b41aa4e60a9012e
Manoel Ívisson <121980133+ManoelIvisson@users.noreply.github.com>
2025-07-29 23:28:40 +0000

    Começando o testbench

---
+++ tb_ula_74181.sv
@@ -0,0 +1,14 @@
+module tb_ula_74181;
+
+logic[3:0] a, b, s;
+logic m, c_in;
+
+ula_74181 uut(.a());
+
+initial
+begin
+    
+    
+    $stop;
+end
+endmodule
--- ula_74181.sv
+++ ula_74181.sv
@@ -1,70 +1,155 @@
 module ula_74181(
     input logic[3:0] a, b, s,
     input logic m, c_in,
-    output logic[3:0] f, c_out
+    output logic[3:0] f, c_out,
+    output logic a_eq_b
 );
 always_comb begin : operacoes
     case (m)
         1'b0: case (c_in)
             1'b0: case (s)
-                4'b0000: f = a + 1;
-                4'b0001: f = a | b + 1;
-                4'b0010: f = a | ~b + 1;
-                4'b0011: f = 0;
-                4'b0100: f = a + (a & ~b) + 1;
-                4'b0101: f = (a | b) + (a & ~b) + 1;
-                4'b0110: f = a - b;
-                4'b0111: f = (a & ~b);
-                4'b1000: f = a + (a & b) + 1;
-                4'b1001: f = a + b + 1;
-                4'b1010: f = (a | ~b) + (a & b) + 1;
-                4'b1011: f = a & b;
-                4'b1100: f = a + a + 1;
-                4'b1101: f = (a | b) + a + 1;
-                4'b1110: f = (a | ~b) + a + 1;
-                4'b1111: f = a;
-                default: f = 4'b0000;
+                4'b0000: {c_out, f} = a + 1;
+                4'b0001: {c_out, f} = a | b + 1;
+                4'b0010: {c_out, f} = a | ~b + 1;
+                4'b0011: begin
+                    f = 0; 
+                    c_out = 0;
+                end
+                4'b0100: {c_out, f} = a + (a & ~b) + 1;
+                4'b0101: {c_out, f} = (a | b) + (a & ~b) + 1;
+                4'b0110: {c_out, f} = a - b;
+                4'b0111: begin
+                    f = (a & ~b); 
+                    c_out = 0;
+                end
+                4'b1000: {c_out, f} = a + (a & b) + 1;
+                4'b1001: {c_out, f} = a + b + 1;
+                4'b1010: {c_out, f} = (a | ~b) + (a & b) + 1;
+                4'b1011: begin 
+                    f = a & b; 
+                    c_out = 0;
+                end
+                4'b1100: {c_out, f} = a + a + 1;
+                4'b1101: {c_out, f} = (a | b) + a + 1;
+                4'b1110: {c_out, f} = (a | ~b) + a + 1;
+                4'b1111: begin 
+                    f = a; 
+                    c_out = 0;
+                end
+                default: begin 
+                    f = 4'b0000; 
+                    c_out = 0;
+                end
             endcase
             1'b1: case (s)
-                4'b0000: f = a;
-                4'b0001: f = a | b;
-                4'b0010: f = a | ~b;
-                4'b0011: f = -1;
-                4'b0100: f = a + (a & ~b);
-                4'b0101: f = (a | b) + (a & ~b);
-                4'b0110: f = a - b - 1;
-                4'b0111: f = (a & ~b) - 1;
-                4'b1000: f = a + (a & b);
-                4'b1001: f = a + b;
-                4'b1010: f = (a | ~b) + (a & b);
-                4'b1011: f = (a & b) - 1;
-                4'b1100: f = a + a;
-                4'b1101: f = (a | b) + a;
-                4'b1110: f = (a | ~b) + a;
-                4'b1111: f = a - 1;
-                default: f = 4'b0000;
+                4'b0000: begin 
+                    f = a; 
+                    c_out = 0;
+                end
+                4'b0001: begin 
+                    f = a | b; 
+                    c_out = 0;
+                end
+                4'b0010: begin 
+                    f = a | ~b; 
+                    c_out = 0;
+                end
+                4'b0011: begin 
+                    f = -1; 
+                    c_out = 0;
+                end
+                4'b0100: {c_out, f} = a + (a & ~b);
+                4'b0101: {c_out, f} = (a | b) + (a & ~b);
+                4'b0110: {c_out, f} = a - b - 1;
+                4'b0111: {c_out, f} = (a & ~b) - 1;
+                4'b1000: {c_out, f} = a + (a & b);
+                4'b1001: {c_out, f} = a + b;
+                4'b1010: {c_out, f} = (a | ~b) + (a & b);
+                4'b1011: {c_out, f} = (a & b) - 1;
+                4'b1100: {c_out, f} = a + a;
+                4'b1101: {c_out, f} = (a | b) + a;
+                4'b1110: {c_out, f} = (a | ~b) + a;
+                4'b1111: {c_out, f} = a - 1;
+                default: begin 
+                    f = 4'b0000; 
+                    c_out = 0;
+                end
             endcase
             default: f = 4'b0000;
         endcase 
         1'b1: case (s)
-            4'b0000: f = ~a;
-            4'b0001: f = ~(a | b);
-            4'b0010: f = ~a & b;
-            4'b0011: f = 0;
-            4'b0100: f = ~(a & b);
-            4'b0101: f = ~b;
-            4'b0110: f = a ^ b;
-            4'b0111: f = a & ~b;
-            4'b1000: f = ~a | b;
-            4'b1001: f = ~(a ^ b);
-            4'b1010: f = b;
-            4'b1011: f = a & b;
-            4'b1100: f = 1;
-            4'b1101: f = a | ~b;
-            4'b1110: f = a | b;
-            4'b1111: f = a;
-            default: f = 4'b0000;
+            4'b0000: begin 
+                f = ~a;
+                c_out = 0;
+            end
+            4'b0001: begin 
+                f = ~(a | b);
+                c_out = 0;
+            end
+            4'b0010: begin 
+                f = ~a & b;
+                c_out = 0;
+            end
+            4'b0011: begin 
+                f = 0;
+                c_out = 0;
+            end
+            4'b0100: begin 
+                f = ~(a & b);
+                c_out = 0;
+            end
+            4'b0101: begin 
+                f = ~b;
+                c_out = 0;
+            end
+            4'b0110: begin 
+                f = a ^ b;
+                c_out = 0;
+            end
+            4'b0111: begin 
+                f = a & ~b;
+                c_out = 0;
+            end
+            4'b1000: begin 
+                f = ~a | b;
+                c_out = 0;
+            end
+            4'b1001: begin 
+                f = ~(a ^ b);
+                c_out = 0;
+            end
+            4'b1010: begin 
+                f = b;
+                c_out = 0;
+            end
+            4'b1011: begin 
+                f = a & b;
+                c_out = 0;
+            end
+            4'b1100: begin 
+                f = 1;
+                c_out = 0;
+            end
+            4'b1101: begin 
+                f = a | ~b;
+                c_out = 0;
+            end
+            4'b1110: begin 
+                f = a | b;
+                c_out = 0;
+            end
+            4'b1111: begin 
+                f = a;
+                c_out = 0;
+            end
+            default: begin 
+                f = 4'b0000;
+                c_out = 0;
+            end
         endcase
     endcase
+
+
+    a_eq_b = a == b;
 end
 endmodule
